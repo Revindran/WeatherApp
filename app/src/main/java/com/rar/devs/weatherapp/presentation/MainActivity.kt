469700d0ef74d177e34a327b082055ce87069d63
Revindran <ravindran1307@gmail.com>
2023-03-11 16:27:14 +0530

    Initial commit

---
+++ app/src/main/java/com/rar/devs/weatherapp/presentation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.rar.devs.weatherapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.rar.devs.weatherapp.presentation.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+            }
+        }
+    }
+}
